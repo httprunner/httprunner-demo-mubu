025e2ed71b48cbf5546ce969071ee6a0876439ef
debugtalk <mail@debugtalk.com>
2019-07-20 16:47:37 +0800

    新增 debugtalk.py

--- debugtalk.py
+++ debugtalk.py
@@ -3,3 +3,9 @@ import time
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+def get_timestamp():
+    return str(int(time.time() * 1000))
+
+def print_documentId(doc_id):
+    print("documentId = {}".format(doc_id))
