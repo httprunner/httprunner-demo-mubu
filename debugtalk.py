e20d97cb8eac64eee339d63f9de864aa9d1c6773
debugtalk <mail@debugtalk.com>
2019-07-20 16:54:34 +0800

    新增 base_url

--- debugtalk.py
+++ debugtalk.py
@@ -9,3 +9,9 @@ def get_timestamp():
 
 def print_documentId(doc_id):
     print("documentId = {}".format(doc_id))
+
+def get_base_url(env_type="prod"):
+    if env_type == "prod":
+        return "https://mubu.com"
+    else:
+        return "https://test.mubu.com"
